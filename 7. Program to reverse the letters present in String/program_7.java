ec69409513ace222ad966137050f2f43e390af92
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:11:58 +0530

    Add files via upload

--- 7. Program to reverse the letters present in String/program_7.java
+++ 7. Program to reverse the letters present in String/program_7.java
@@ -1,1 +1,26 @@
+package Tathastu_Internship;
+import java.io.*;
+import java.util.Scanner;
+public class program_7
+{
+    //Java Program to reverse the letters present in the given String.
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String str= sc.next() ,nstr="";
+        char ch;
+
+        System.out.print("Original word: ");
+        System.out.println(str); //Example word
+
+        for (int i=0; i<str.length(); i++)
+        {
+            ch= str.charAt(i); //extracts each character
+            nstr= ch+nstr; //adds each character in front of the existing string
+        }
+        System.out.println("Reversed word: "+ nstr);
+    }
+}
+
 
