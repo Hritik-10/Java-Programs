5328d9b5dc11ab765aa2cbce5a44894fa3b6a718
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:05:51 +0530

    Add files via upload

--- 4. String Palindrome/program_4.java
+++ 4. String Palindrome/program_4.java
@@ -1,1 +1,31 @@
+package Tathastu_Internship;
 
+import java.util.Scanner;
+
+//java program to find out given string is palindrome or not
+public class program_4
+{
+    public static boolean is_palindrome(String str)
+    {
+        String st="";
+        for(int i=0;i<str.length();i++)
+        {
+            st=str.charAt(i)+st;
+        }
+
+        if(st.equalsIgnoreCase(str))
+            return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str=sc.next();
+        boolean ans=is_palindrome(str);
+        if(ans==true)
+            System.out.println("string is palindrome");
+        else
+            System.out.println("string is not palindrome");
+    }
+}
