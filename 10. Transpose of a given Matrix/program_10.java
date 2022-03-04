6c28d9fb66416f785ac08afd228bb3abc971d7b9
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:15:36 +0530

    Add files via upload

--- 10. Transpose of a given Matrix/program_10.java
+++ 10. Transpose of a given Matrix/program_10.java
@@ -1,1 +1,33 @@
+package Tathastu_Internship;
 
+//Java Program to find the Transpose of a given Matrix.
+public class program_10
+{
+    public static void main(String args[])
+    {
+        int original[][]={{1,7,4},{3,4,5},{4,5,5}};
+        int transpose[][]=new int[3][3];  //3 rows and 3 columns
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                transpose[i][j]=original[j][i];
+            }
+        }
+
+        System.out.println("Printing Matrix without transpose:");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(original[i][j]+" ");
+            }
+            System.out.println();//new line
+        }
+        System.out.println("Printing Matrix After Transpose:");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(transpose[i][j]+" ");
+            }
+            System.out.println();//new line
+        }
+    }
+}
