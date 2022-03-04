7e6fc7445a4b00cc5d5fd9253f3982056d910770
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:03:31 +0530

    Add files via upload

--- 2.Fibonacci Series/program_2.java
+++ 2.Fibonacci Series/program_2.java
@@ -1,1 +1,29 @@
+package Tathastu_Internship;
 
+import java.util.Scanner;
+
+//java program to calculate Fibonacci Series up to n numbers.
+public class program_2
+{
+    public static void fibonacci(int num)
+    {
+       int n1=0,n2=1,i,n3,count=num;
+        System.out.print(n1+" "+n2);  //for printing 0 and 1
+        for(i=2;i<count;i++)
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+    }
+    public static void main(String[] args)
+    {
+        //java program to calculate fibonacii series upto n numbers
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num=sc.nextInt();
+        System.out.println("Fibonacci Series till given number is:");
+        fibonacci(num);
+    }
+}
