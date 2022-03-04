171cdd96eba1e6899dde2367009ed166f9509305
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:06:55 +0530

    Add files via upload

--- Permutation and Combination/program_5.java
+++ Permutation and Combination/program_5.java
@@ -1,1 +1,47 @@
+package Tathastu_Internship;
+import java.util.*;
 
+//java program to calculate the permutation and combination of two numbers
+public class program_5
+{
+    final static Scanner sc=new Scanner(System.in);
+        static int fact(int number) {
+                int f = 1;
+                int j = 1;
+                while (j <= number) {
+                    f = f * j;
+                    j++;
+                }
+                return f;
+            }
+
+            public static void main(String args[])
+            {
+                List<Integer> numbers = new ArrayList<Integer>();
+
+                numbers.add(12);
+                numbers.add(13);
+
+
+                int n = numbers.size();
+                System.out.println("n="+ n);
+                System.out.print("Enter the value of r:");
+                int r = sc.nextInt();
+                int result;
+
+                System.out.print("Given array is: ");
+                for(int i=0;i<numbers.size();i++)
+                {
+                    System.out.print(numbers.get(i)+" ");
+                }
+                System.out.println();
+
+                //permutation
+                result = fact(n) / fact(n-r);
+                System.out.println("The permutation value for "+n+"P"+r+" is: " + result);
+
+                //combination
+                result = fact(n) / (fact(r) * fact(n-r));
+                System.out.println("The combination value for "+n+"C"+r+" is: " + result);
+            }
+}
