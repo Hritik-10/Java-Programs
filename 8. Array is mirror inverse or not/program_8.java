833b79611845c8f5ed4eb19db817fd5b7d945ce9
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:13:40 +0530

    Add files via upload

--- 8. Array is mirror inverse or not/program_8.java
+++ 8. Array is mirror inverse or not/program_8.java
@@ -1,1 +1,28 @@
+package Tathastu_Internship;
+
+public class program_8
+{
+    //program to check whether the given array is mirror inverse or not
+    static boolean isMirrorInverse(int arr[])
+    {
+        for (int i = 0; i < arr.length; i++) {
+            // If condition fails for any element
+            if (arr[arr[i]] != i)
+                return false;
+        }
+
+            // Given array is mirror-inverse
+            return true;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 2, 4, 3 };
+        if (isMirrorInverse(arr))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
 
