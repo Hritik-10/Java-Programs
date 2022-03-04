2b310c42e264c373605bcff19b8808159357d763
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:14:40 +0530

    Add files via upload

--- 9. program to remove elements from an ArrayList/program_9.java
+++ 9. program to remove elements from an ArrayList/program_9.java
@@ -1,1 +1,29 @@
+package Tathastu_Internship;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//Java program to remove elements from an ArrayList
+public class program_9
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        List<Integer> alist = new ArrayList<>();
+
+        alist.add(10);
+        alist.add(20);
+        alist.add(30);
+        alist.add(1);
+        alist.add(2);
+
+        System.out.println(alist);
+
+        System.out.print("Enter the index of removing element:");
+        int ind=sc.nextInt();
+        alist.remove(ind);
+        System.out.println("Elements in list after removing given element: "+alist);
+    }
+}
+
 
