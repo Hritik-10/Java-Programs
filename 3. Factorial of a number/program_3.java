9a0ea6fdcff613e303f8f23af61ec570a5bc7a3b
Hritik Santosh Bansode <84392606+Hritik-10@users.noreply.github.com>
2022-03-05 00:04:22 +0530

    Add files via upload

--- 3. Factorial of a number/program_3.java
+++ 3. Factorial of a number/program_3.java
@@ -1,1 +1,24 @@
+package Tathastu_Internship;
 
+import java.util.Scanner;
+
+//Java program to calculate a Factorial of a number.
+public class program_3
+{
+    static int factorial(int n)
+    {
+        if(n==0)
+            return 1;
+        else
+            return(n*factorial(n-1));
+    }
+
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num=sc.nextInt();
+        System.out.println("Factorial of given number is: "+ factorial(num));
+    }
+}
